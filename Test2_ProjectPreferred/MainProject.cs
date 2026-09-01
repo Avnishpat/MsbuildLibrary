697403d446a8262e928057175c5ae5edcd4185a4
Avnish Patel <110018459+Avnishpat@users.noreply.github.com>
2026-09-01 11:26:33 +0530

    Create MainProject.cs

---
+++ Test2_ProjectPreferred/MainProject.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MainProject
+{
+    public class MainProjectClass
+    {
+        public static string GetBuildType()
+        {
+            return "REAL PROJECT";
+        }
+    }
+}
